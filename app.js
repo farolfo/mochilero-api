d3aabf2a8c265ab5bce3a262239aeb33f33147d4
Franco <francoarolfo@hotmail.com>
2015-02-16 03:37:24 -0300

    added cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://farolfo.github.io');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
